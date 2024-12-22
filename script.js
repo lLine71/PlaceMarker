5d01d204125f70bbb2212352e24575308a9c3353
Lev Vysotscky <vysotscky.leva@yandex.com>
2024-12-22 22:20:01 +0300

    MarkerTestButtons14

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function route1(){
+function router(value){
 	const arrow1 = document.getElementById("arr1");
 	
-	arrow1.object3D.rotation.set(Math.PI / 2, 0, Math.PI / 4);
+	if (value == '1'){
+		alert('Rotated');
+		arrow1.object3D.rotation.set(Math.PI / 2, 0, Math.PI / 4);
+	}
+	
 }
 
 window.onload = () => {
-	const marker1 = document.getElementById('animated-marker1');
+	//const marker1 = document.getElementById('animated-marker1');
 	
 	
 	pujs.popup(title = 'Введите место назначения', 
 	message = '', 
-	buttons = [{ 'text': 'Ввод', callback: (value) => {alert(value)} },
+	buttons = [{ 'text': 'Ввод', callback: (value) => {router(value)} },
 	{ 'text': 'Отмена', callback: () => { }, color: 'var(--pu-red)' }], 
 	'vert', [{placeholder: ''}])
 };
