282c10825a31fc3e2a340f4c9eac75d03901c48a
Lev Vysotscky <vysotscky.leva@yandex.com>
2024-12-22 20:00:07 +0300

    MarkerTest3

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 window.onload = () => {
-	const marker1 = document.getElemntById('animated-marker1');
-	const arrow1 = document.querySelector("arr1");
+	const marker1 = document.getElementById('animated-marker1');
+	const arrow1 = document.getElementById("arr1");
 	
 	arrow1.object3D.rotation.set(THREE.MathUtils.degToRad(15), THREE.MathUtils.degToRad(30), THREE.MathUtils.degToRad(90));
-	alert('hopeitrotated')
+	
+	pujs.popup(title = 'Choose your destination', message = '', 
+	buttons = [{ 'text': 'Action', callback: (value) => { 
+	setTimeout(()=>{console.log(value); pujs.alert(JSON.stringify(value), 'success')},1) } },{ 'text': 'Cancel', 
+	callback: () => { }, color: 'var(--pu-red)' }], 'vert', [{placeholder: 'Try type something'}, {placeholder: 
+	'Hidden Text', type: 'password'}])
+
 };
