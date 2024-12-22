9bbb16857cc0bfa3e36428e7fd62e1c31127a3ac
Lev Vysotscky <vysotscky.leva@yandex.com>
2024-12-22 21:18:21 +0300

    MarkerTestButtons1

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-
-
-window.onload = () => {
-	const marker1 = document.getElementById('animated-marker1');
+function route1(){
 	const arrow1 = document.getElementById("arr1");
 	
 	arrow1.object3D.rotation.set(Math.PI / 2, 0, Math.PI / 4);
+}
+
+window.onload = () => {
+	//const marker1 = document.getElementById('animated-marker1');
+	
 	
 	pujs.popup(title = 'Choose your destination', message = '', 
-	buttons = [{ 'text': 'Action', callback: (value) => { 
-	setTimeout(()=>{console.log(value); pujs.alert(JSON.stringify(value), 'success')},1) } },{ 'text': 'Cancel', 
-	callback: () => { }, color: 'var(--pu-red)' }], 'vert', [{placeholder: 'Try type something'}, {placeholder: 
-	'Hidden Text', type: 'password'}])
+	buttons = [{'vert', [{placeholder: 'Try type something'}])
+	//buttons = [{ 'text': 'Action', callback: (value) => { 
+	//setTimeout(()=>{console.log(value); pujs.alert(JSON.stringify(value), 'success')},1) } },{ 'text': 'Cancel', 
+	//callback: () => { }, color: 'var(--pu-red)' }], 'vert', [{placeholder: 'Try type something'}, {placeholder: 
+	//'Hidden Text', type: 'password'}])
 
 };
