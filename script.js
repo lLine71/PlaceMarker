5d3278299b06334c697a88f1b723c6d6a5dec511
Lev Vysotscky <vysotscky.leva@yandex.com>
2024-12-22 21:19:51 +0300

    MarkerTestButtons2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function route1(){
 	const arrow1 = document.getElementById("arr1");
 	
 	arrow1.object3D.rotation.set(Math.PI / 2, 0, Math.PI / 4);
-}
+};
 
 window.onload = () => {
 	//const marker1 = document.getElementById('animated-marker1');
